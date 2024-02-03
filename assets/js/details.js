da46f81d726c2fe50e31d9e096a0cea184cd71f3
Qusaynairat <qusaynairat123@yahoo.com>
2024-02-03 16:51:30 +0200

    change

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,17 +1,43 @@
-
 async function getproduct() {
     const urlParams = new URLSearchParams(window.location.search);
     const q = urlParams.get("m_id");
+    console.log(q);
     const response = await fetch(`https://dummyjson.com/products/${q}`);
     const data = await response.json();
     console.log(data);
-    const {title, description, price,rating,brand,thumbnail} = data;
-    document.querySelector(".title").innerHTML=title;
-    document.querySelector(".description").innerHTML="Description: "+description;
-    document.querySelector(".price").innerHTML="Price: "+price+" $";
-    document.querySelector(".rating").innerHTML="Rating: "+rating;
-    document.querySelector(".brand").innerHTML="Brand: "+brand;
-    document.querySelector(".image").src=thumbnail;
-    
-    }
+    const { title, description, price, rating, brand, thumbnail,images } = data;
+    document.querySelector(".title").innerHTML = title;
+    document.querySelector(".description").innerHTML = "Description: " + description;
+    document.querySelector(".price").innerHTML = "Price: " + price + " $";
+    document.querySelector(".rating").innerHTML = "Rating: " + rating;
+    document.querySelector(".brand").innerHTML = "Brand: " + brand;
+    const image =images.map((img) => {
+        return `<div class="swiper-slide"><img src="${img}" alt=""></div>`;
+    });
+    document.querySelector(".swiper-wrapper").innerHTML = image.join("");
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+}
 getproduct();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// import axios from 'axios';
 async function getproducts() {
     const response = await fetch("https://dummyjson.com/products");
     const data = await response.json();
@@ -5,7 +6,6 @@ async function getproducts() {
     const mobiles = data.products;
     console.log(mobiles);
     const result = mobiles.map((mobile) => {
-        
         return `
         <div class="product">
         <img src="${mobile.thumbnail}" alt="">
@@ -14,7 +14,38 @@ async function getproducts() {
         <h4><span>Brand : </span>${mobile.brand}</h4>
         <a href="details.html?m_id=${mobile.id}">Details</a>
         </div>`
-});
-document.querySelector(".products").innerHTML=result.join(""); 
+    });
+
+    document.querySelector(".products").innerHTML = result.join("");
+    const img = data.products.map((img) => {
+        return `<div class="swiper-slide"><img src="${img.thumbnail}" alt=""></div>`;
+    });
+    document.querySelector(".swiper-wrapper").innerHTML = img.join("");
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+        clickable: true,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
+        // Navigation arrows
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
 }
 getproducts();
